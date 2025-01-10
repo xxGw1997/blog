35a9ae257f8cb2052920849c740d9ee7206af8df
yangwenxuan <540040588@qq.com>
2025-01-10 22:24:29 +0800

    feat: add custom img zoom in dialog

--- src/components/mdx/custom-image.tsx
+++ src/components/mdx/custom-image.tsx
@@ -1,4 +1,15 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/components/ui/dialog";
 
 interface CustomImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
   [key: string]: any;
@@ -11,5 +22,20 @@ export const CustomImage = ({
   height,
   ...rest
 }: CustomImageProps) => {
-  return <img alt={alt} src={src} width={width} height={height} {...rest} />;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger>
+        <img alt={alt} src={src} width={width} height={height} {...rest}  className="cursor-zoom-in"/>;
+      </DialogTrigger>
+      <DialogContent onInteractOutside={(e) => e.preventDefault()} className="max-w-[90%] max-h-[80vh] overflow-y-scroll">
+        <DialogHeader>
+          <DialogTitle></DialogTitle>
+          <DialogDescription></DialogDescription>
+        </DialogHeader>
+        <img src={src} alt={alt} className="w-full" />
+      </DialogContent>
+    </Dialog>
+  );
 };
